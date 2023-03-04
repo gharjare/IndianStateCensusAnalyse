82eaea451b8fe6a62eec620c5524d1dbd3a5d384
gharjare <gharjarechetan@gmail.com>
2023-03-04 11:19:17 +0530

    [chetan gharjare] initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateAnalyser
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian census Analyser");
+        }
+    }
+}
